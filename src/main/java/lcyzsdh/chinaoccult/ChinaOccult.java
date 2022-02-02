6a2e8aa9ee73235914d60f6a8ac571f5dc27175b
lcyzsdh <1151468261@qq.com>
2022-02-02 21:04:51 +0800

    Init2.

--- src/main/java/lcyzsdh/chinaoccult/ChinaOccult.java
+++ src/main/java/lcyzsdh/chinaoccult/ChinaOccult.java
@@ -1,8 +1,12 @@
 package lcyzsdh.chinaoccult;
 
+import lcyzsdh.chinaoccult.items.ItemRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Utils.MOD_ID)
 public class ChinaOccult {
-
+    public ChinaOccult(){
+        ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
 }
---
+++ src/main/java/lcyzsdh/chinaoccult/items/BlackSteelIngot.java
@@ -0,0 +1,10 @@
+package lcyzsdh.chinaoccult.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class BlackSteelIngot extends Item {
+    public BlackSteelIngot() {
+        super(new Properties().tab(ItemGroup.TAB_MATERIALS));
+    }
+}
